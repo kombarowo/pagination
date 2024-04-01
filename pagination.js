a2abba479d9d1bbff4ad6ff2c0a73e870319eed0
Artyom Kombarov <artyom.kombaroff@gmail.com>
2024-04-02 00:57:41 +0300

    Remastering.

--- pagination.js
+++ pagination.js
@@ -1,177 +1,23 @@
-/**
- * @param {Object} options
- * @param {number|string} options.total total pages
- * @param {number|string} options.current current page
- * @param {number|string} options.pageRange count of displayed pages
- * @returns {Object} pagination object
- */
-function getPaginationItems({ total = 10, current = 6, pageRange = 3 } = {}) {
-  const DOTS = 2;
-  const LAST_INDEX = total + DOTS - 1;
+const arrayRange = (from, n) => Array.from({ length: n }, (v, i) => from + i);
 
-  let items = [];
+const pagination = (cur, total, n = 3) => {
+    const from = Math.min(total - n, Math.max(2, cur - Math.trunc(n / 2)));
 
-  total = parseInt(total);
-  current = parseInt(current);
-  pageRange = parseInt(pageRange);
+    var arr1 = [1, '...'],
+        arr2 = [],
+        arr3 = ['...', total];
 
-  pageRange += DOTS;
-  if (pageRange < 1) { 1 + DOTS };
-
-  // simple case
-  if (total <= pageRange + DOTS) {
-
-    for (let i = 0; i < total; i++) {
-      let page = {
-        content: i + 1,
-        show: false
-      };
-
-      items.push(page);
+    if (total <= arr1.length + n + arr3.length) {
+        return arrayRange(1, total);
+    } else if (from - 2 <= 1) {
+        arr1.pop();
+        arr2 = arrayRange(from - (from - 2), n + 1);
+    } else if (total - from - n <= 1) {
+        arr3.shift();
+        arr2 = arrayRange(from - 1 + (total - from - n), n + 1);
+    } else {
+        arr2 = arrayRange(from, n);
     }
 
-    for (let i = 0; i < total; i++) {
-
-      items[i].show = true;
-
-    }
-
-    const result = items.filter(i => i.show).map(i => {
-
-      delete i.show;
-
-      if (i.content.toString().trim() === current.toString().trim()) {
-        i.active = true;
-      }
-
-      return i;
-    });
-
-    return result;
-  }
-
-  // main case
-
-  let page1 = {
-    content: "...",
-    show: false
-  };
-
-  let page2 = {
-    content: "...",
-    show: false
-  };
-
-  //fill all values
-  for (let i = 0; i < total; i++) {
-
-    let page = {
-      content: i + 1,
-      show: false
-    };
-
-    items.push(page);
-
-  }
-
-  //add dots
-  items.splice(1, 0, page1);
-  items.splice(items.length - 1, 0, page2);
-
-  //conditions
-  items[0].show = true;
-  items[LAST_INDEX].show = true;
-
-  for (let i = 0; i < items.length; i++) {
-    //shuffle in start
-    if (current < pageRange) {
-      page1.show = false;
-      page2.show = true;
-
-      if (i <= pageRange) {
-        items[i].show = true;
-      }
-    }
-
-    if (current >= pageRange && pageRange - DOTS > 2) {
-
-      page1.show = true;
-
-      //shuffle in the middle
-      if (current < total - pageRange + DOTS) {
-        page2.show = true;
-        const delt = current - Math.floor(pageRange / 2);
-
-        if (i > delt && i <= ((pageRange - DOTS) + delt)) {
-          items[i].show = true;
-        }
-
-      }
-      //shuffle in the end
-      else {
-        page2.show = false;
-        const delt = total - pageRange;
-
-        if (i > delt) {
-          items[i].show = true;
-        }
-      }
-    } else if (current === pageRange && pageRange - DOTS >= 1) {
-      page1.show = false;
-      page2.show = true;
-
-      if (i <= pageRange) {
-        items[i].show = true;
-      }
-    } else if (current > pageRange && pageRange - DOTS >= 1) {
-      page1.show = true;
-
-      //shuffle in the middle
-      if (current <= total - pageRange) {
-        page2.show = true;
-        const delt = current - Math.floor(pageRange / 2);
-
-        if (i > delt && i <= ((pageRange - DOTS) + delt)) {
-          items[i].show = true;
-        }
-
-      }
-      //shuffle in the end
-      else {
-        page2.show = false;
-        const delt = total - pageRange;
-
-        if (i > delt) {
-          items[i].show = true;
-        }
-      }
-    }
-  }
-
-  const result = items.filter(i => i.show).map(i => {
-
-    delete i.show;
-
-    if (i.content.toString().trim() === current.toString().trim()) {
-      i.active = true;
-    }
-
-    return i;
-  });
-
-  return result;
-}
-
-/* Render to HTML */
-let template = '';
-const items = getPaginationItems();
-
-items.forEach(item => {
-
-  const string = `<span style="padding: 10px; font-size: 26px; ${item.active ? 'color:red;' : ''}" >${item.content}</span>`
-
-  template += string;
-
-});
-
-document.getElementById('pagination').innerHTML = template;
+    return [...arr1, ...arr2, ...arr3];
+};
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
-**Custom JSON pagination**
+# Pagination
+
+Get pagination array with overflow dots
+
+```
+pagination(10, 20, 5)
+```
+
+**Params:**
+
+-   current page
+-   total(last page)
+-   n, how many pages to show between _first_ and _total_
